4a02ff3bba8622150498b60ed6a068080aeb5f17
Sarah Leong <sleong20@stuy.edu>
2021-09-22 16:04:28 -0400

    added problem5 code

--- work3.c
+++ work3.c
@@ -3,11 +3,9 @@ int problem1();
 int problem5();
 
 int main() {
-    int a1 = problem1();
-    // int a5 = problem5();
 
-    printf("Problem 1 Answer: %d \n", a1);
-    printf("Problem 5 Answer: %d \n", problem1());
+    printf("Problem 1 Answer: %d \n", problem1());
+    printf("Problem 5 Answer: %d \n", problem5());
 
     return 0;
 }
@@ -21,4 +19,19 @@ int problem1() {
         }
     }
     return sum;
+}
+
+int problem5() {
+    int ans;
+    int found = 1;
+    int x;
+    for (ans=2520; found != 0; ans++) {
+        found = 0;
+        for (x = 1; x < 21; x++) {
+            if (ans % x != 0) {
+                found = 0;
+            }
+        }
+    }
+    return ans;
 }
