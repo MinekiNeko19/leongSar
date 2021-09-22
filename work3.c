09115944f19a401a20eb032c86a3676503f0159d
Sarah Leong <sleong20@stuy.edu>
2021-09-22 15:10:32 -0400

    finished problem 1

--- work3.c
+++ work3.c
@@ -1,19 +1,20 @@
-#include <stdio.c>
+#include <stdio.h>
 int problem1();
 int problem5();
 
 int main() {
     int a1 = problem1();
-    int a5 = problem5();
+    // int a5 = problem5();
 
     printf("Problem 1 Answer: %d \n", a1);
-    printf("Problem 5 Answer: %d \n", a5);
+    printf("Problem 5 Answer: %d \n", problem1());
 
     return 0;
 }
 
 int problem1() {
     int sum = 0;
+    int x;
     for (x = 0; x < 1000; x++) {
         if ((x % 3 == 0) || (x % 5 == 0)) {
             sum += x;
