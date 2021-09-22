b4a926f942c157136708345784704aa4183103fd
Sarah Leong <sleong20@stuy.edu>
2021-09-22 15:08:19 -0400

    added the work from class onto this file

---
+++ work3.c
@@ -0,0 +1,23 @@
+#include <stdio.c>
+int problem1();
+int problem5();
+
+int main() {
+    int a1 = problem1();
+    int a5 = problem5();
+
+    printf("Problem 1 Answer: %d \n", a1);
+    printf("Problem 5 Answer: %d \n", a5);
+
+    return 0;
+}
+
+int problem1() {
+    int sum = 0;
+    for (x = 0; x < 1000; x++) {
+        if ((x % 3 == 0) || (x % 5 == 0)) {
+            sum += x;
+        }
+    }
+    return sum;
+}
